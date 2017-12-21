d5eb3e18a9ea63410545586a7e6f2e51a8267b40
Jonathan Moffitt <moffitts1@yahoo.com>
2017-12-21 12:26:12 -0500

    Added user api

---
+++ api/models/usermodel.js
@@ -0,0 +1,39 @@
+'use strict';
+var mongoose = require("mongoose");
+var schema = mongoose.Schema;
+
+var userSchema = new schema({
+    first_name: {
+        type: String,
+        required: "Please enter your first name"
+    },
+    last_name: {
+        type: String,
+        required: "Please enter your last name"
+    },
+    email: {
+        type: String,
+        required: "Please enter your email address"
+    },
+    home_phone: {
+        type: String,
+        required: "Please enter a home phone number"
+    },
+    cell_phone: {
+        type: String,
+        required: "Please enter a cell phone number"
+    },
+    best_contact_method: {
+        type: [{
+            type: String,
+            enum: ["email", "home phone", "cell phone", "facebook"]
+        }],
+        default: ["email"]
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model("Users", userSchema);
---
+++ api/routes/userroutes.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(app) {
+    var users = require("../controllers/usercontroller");
+
+    app.route("/users")
+        .get(users.list_all_users)
+        .post(users.create_user);
+
+    app.route("/user/:userId")
+        .get(users.get_user_details)
+        .put(users.update_user)
+        .delete(users.delete_user);
+};
